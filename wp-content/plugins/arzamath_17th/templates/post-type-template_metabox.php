a36271afd4938aa9168f1fba130eaa5685b6884a
SuddyaTOXA <suddyadred@gmail.com>
2014-12-14 17:08:58 +0200

    14.12.14

--- wp-content/plugins/arzamath_17th/templates/post-type-template_metabox.php
+++ wp-content/plugins/arzamath_17th/templates/post-type-template_metabox.php
@@ -15,16 +15,6 @@
 <!--            <input type="text" id="meta-multi-select" name="meta-multi-select" value="--><?php //echo @get_post_meta($post->ID, 'meta-multi-select', true); ?><!--" />-->
            <?php $options_multi_select = @get_post_meta($post->ID, 'meta-multi-select', true); ?>
             <select id='meta-multi-select' name='meta-multi-select[multi-select-box][]' multiple size="5"  >
-<!--                <option value="1">Один</option>-->
-<!--                <option value="2">Два</option>-->
-<!--                <option value="3">Три</option>-->
-<!--                <option value="4">Чотири</option>-->
-<!--                <option value="5">П’ять</option>-->c1
-<!--                <option value='1' --><?php //if ( $options_multi_select['multi-select-box'] == 1 ) echo 'selected="selected"' ; ?><!-->Один</option>-->
-<!--                <option value='2' --><?php //if ( $options_multi_select['multi-select-box'] == 2 ) echo 'selected="selected"' ; ?><!-->Два</option>-->
-<!--                <option value='3' --><?php //if ( $options_multi_select['multi-select-box'] == 3 ) echo 'selected="selected"' ; ?><!-->Три</option>-->
-<!--                <option value='4' --><?php //if ( $options_multi_select['multi-select-box'] == 4 ) echo 'selected="selected"' ; ?><!-->Чотири</option>-->
-<!--                <option value='5' --><?php //if ( $options_multi_select['multi-select-box'] == 5 ) echo 'selected="selected"' ; ?><!-->П’ять</option>-->
                 <option value='1' <?php selected( in_array("1", $options_multi_select['multi-select-box'] ) ); ?>>Один</option>
                 <option value='2' <?php selected( in_array("2", $options_multi_select['multi-select-box'] ) ); ?>>Два</option>
                 <option value='3' <?php selected( in_array("3", $options_multi_select['multi-select-box'] ) ); ?>>Три</option>
